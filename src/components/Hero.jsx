2d994fd63c370091de54ff4f9ce1dbd5e4c9989d
Fabienne Muff <fabienne.muff@sunrise.net>
2022-10-26 11:46:21 +0200

    added github api link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,7 +57,7 @@ export function Hero() {
               </p>
               <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
                 <Button href="/">Get started</Button>
-                <Button href="/" variant="secondary">
+                <Button href="https://github.com/Climacrux/api.cdrplatform.com" variant="secondary">
                   View on GitHub
                 </Button>
               </div>
