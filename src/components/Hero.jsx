98b537cfa7c2f0a02d19c949e02f1bccf4b5e721
Fabienne Muff <fabienne.muff@sunrise.net>
2022-11-09 19:07:45 +0100

    adding sizes to image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -85,6 +85,9 @@ export function Hero() {
                 height={530}
                 unoptimized
                 priority
+                sizes="(max-width: 768px) 100vw,
+                (max-width: 1200px) 50vw,
+                33vw"
               />
               <Image
                 className="absolute -bottom-40 -right-44"
