e407bc5009a63aadb09c257628ba497713338b01
Fabienne Muff <fabienne.muff@sunrise.net>
2022-11-09 18:31:06 +0100

    adding "unoptimized" back as it is not helping

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,6 +47,7 @@ export function Hero() {
               alt=""
               width={530}
               height={530}
+              unoptimized
               priority
             />
             <div className="relative">
@@ -79,6 +80,7 @@ export function Hero() {
                 alt=""
                 width={530}
                 height={530}
+                unoptimized
                 priority
               />
               <Image
@@ -87,6 +89,7 @@ export function Hero() {
                 alt=""
                 width={567}
                 height={567}
+                unoptimized
                 priority
               />
               <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10 blur-lg" />
